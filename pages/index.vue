757a21a3d496459c72187712a0310d7be491a7c3
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-08-07 16:07:48 +0200

    done

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Here is how the received payload is looking (Array of Objects)
+    <pre>{{ users }}</pre>
+    <div>
+      -------------
+      <p v-for="user in users" :key="user.id">
+        name: <span>{{ user.name }}</span> and email: {{ user.email }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async fetch() {
+    // $get is a shortcut for `.data` as shown here: https://axios.nuxtjs.org/usage#-shortcuts
+    const usersResult = await this.$axios.$get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    console.log('what we have received', usersResult)
+    this.users = usersResult
+  },
+}
+</script>
